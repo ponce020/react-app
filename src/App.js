83a6d0ff2d0cab5f4feac1e63e60c33271d45d9c
noe <55969280+ponce020@users.noreply.github.com>
2021-03-06 14:00:53 -0600

    StoryReel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
@@ -14,6 +15,7 @@ function App() {
       <Sidebar/>
 
       {/* Content */}
+      <Feed/>
       </div>
 
       
---
+++ src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Story from './Story';
+import './StoryReel.css';
+
+function StoryReel() {
+    return (
+        <div className='storyReel'>
+            {/* Story */}
+            <Story
+                profileSrc='http://t2.gstatic.com/images?q=tbn:ANd9GcS5FMF0eHyYx-OplkuNOhr9i0yFBSYfswPqOG9DlBqKE8NCm8rCuIaxxobU1FHE'
+                image='https://64.media.tumblr.com/012f0804ddf950683235d4426c7354ad/tumblr_owyklh9M6J1qflgwpo1_1280.jpg'
+                title='Miranda Otto'
+            />
+            <Story
+                profileSrc='http://t2.gstatic.com/images?q=tbn:ANd9GcRfLRT5n50IJmIarjAw-rY7mITTMdSAxTOOzKbLrcoJymcqQGwqbLhxAL0m8zkT'
+                image='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a8fd609b-daf2-4dbb-84bf-a881e5c98834/dbkwl2j-e0d8b27b-5404-4165-af06-787e771ca8b6.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYThmZDYwOWItZGFmMi00ZGJiLTg0YmYtYTg4MWU1Yzk4ODM0XC9kYmt3bDJqLWUwZDhiMjdiLTU0MDQtNDE2NS1hZjA2LTc4N2U3NzFjYThiNi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.g6001VijlzrX0wqWMoYTllNRUmoIVI_ObcO0b8PHxW0'
+                title='Karl Urban'
+            />
+            <Story
+                profileSrc='http://t2.gstatic.com/images?q=tbn:ANd9GcS5FMF0eHyYx-OplkuNOhr9i0yFBSYfswPqOG9DlBqKE8NCm8rCuIaxxobU1FHE'
+                image='https://64.media.tumblr.com/012f0804ddf950683235d4426c7354ad/tumblr_owyklh9M6J1qflgwpo1_1280.jpg'
+                title='Miranda Otto'
+            />
+            <Story
+                profileSrc='http://t2.gstatic.com/images?q=tbn:ANd9GcRfLRT5n50IJmIarjAw-rY7mITTMdSAxTOOzKbLrcoJymcqQGwqbLhxAL0m8zkT'
+                image='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a8fd609b-daf2-4dbb-84bf-a881e5c98834/dbkwl2j-e0d8b27b-5404-4165-af06-787e771ca8b6.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYThmZDYwOWItZGFmMi00ZGJiLTg0YmYtYTg4MWU1Yzk4ODM0XC9kYmt3bDJqLWUwZDhiMjdiLTU0MDQtNDE2NS1hZjA2LTc4N2U3NzFjYThiNi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.g6001VijlzrX0wqWMoYTllNRUmoIVI_ObcO0b8PHxW0'
+                title='Karl Urban'
+            />
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+        </div>
+    )
+}
+
+export default StoryReel
