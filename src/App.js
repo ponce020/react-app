eb3008492cb7639c1635633dca6bd25033b8beb8
noe <55969280+ponce020@users.noreply.github.com>
2021-03-05 18:19:16 -0600

    Clean project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <p>Hola</p>
+      {/* Header */}
+
+      {/* Body */}
+
+      {/* Sidebar */}
+
+      {/* Content */}
     </div>
   );
 }
