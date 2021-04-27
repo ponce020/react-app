a8c29392f47bf26dfdaeced576290c97dd47d69f
noe <55969280+ponce020@users.noreply.github.com>
2021-04-27 11:52:02 -0600

    False

---
+++ src/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomePage.css';
+import FormLogin from './FormLogin';
+
+
+function HomePage() {
+    return (
+        <div className='main'>
+            <div className='glass'>
+                <FormLogin/>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
