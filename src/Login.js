b930f805f33f7e14147532d6859dfea4ea617ebd
noe <55969280+ponce020@users.noreply.github.com>
2021-05-01 13:03:32 -0600

    Connection with Firebase and Auth Login

--- src/Login.js
+++ src/Login.js
@@ -1,28 +1,36 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
-import './Login.css'
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "./firebase";
 
 function Login() {
+  const signIn = () => {
+    //sing in...
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => alert(error.mesaage));
+  };
 
-const signIn = () => {
-    //sing in..
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+          alt=""
+        />
+        <img
+          src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg"
+          alt=""
+        />
+      </div>
+      <Button type="submit" onClick={signIn}>
+        Sign in
+      </Button>
+    </div>
+  );
 }
 
-    return (
-        <div className='login'>
-            <div className='login__logo'>
-                <img
-                src='https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg'
-                alt=''
-                />
-                <img
-                src='https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg'
-                alt=''
-                />
-            </div>
-            <Button type='submit' onClick={signIn}>Sign in</Button>
-        </div>
-    )
-}
-
-export default Login
+export default Login;
